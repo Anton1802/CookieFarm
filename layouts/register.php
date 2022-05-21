2d5cd843094c1d17fbc0f676bcc6def2f2a62e25
remote <remote1802@gmail.com>
2022-05-21 20:41:36 +0300

    layouts

---
+++ layouts/register.php
@@ -0,0 +1,10 @@
+<div class="auth">
+    <h1>Reg</h1>
+    <form action="" method="POST">
+        <label>Login:</label>
+        <input type="text" name="login">
+        <label>Password:</label>
+        <input type="text" name="password">
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</div>
