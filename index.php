a0051b5ac14caed23b741501f71cd394922ee0d0
Anton1802 <anton.sup1802@gmail.com>
2022-05-25 04:08:39 +0300

    edit index

--- index.php
+++ index.php
@@ -1,25 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="resource/css/main.css">
-    <link rel="shortcut icon" href="resource/img/cookie.png">
-    <title>CookieFarm</title>
-</head>
-<body>
-    <?php
+<?php
 
-    require_once('layouts/header.php');
+session_start();
 
-    require_once('layouts/mycookies.php');
+require_once('Router/Router.php');
 
-    require_once('layouts/profile.php');
+require_once('config/routes.php');
 
-    require_once('layouts/footer.php');
+use Router\Router;
 
-    ?>
-</body>
-<script src="/resource/js/burger.js" charset="utf-8"></script>
-</html>
+Router::execute($_SERVER['REQUEST_URI']);
