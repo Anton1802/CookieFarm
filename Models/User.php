74710287e5f662c4e387973b0da581eac46d1732
Anton1802 <anton.sup1802@gmail.com>
2022-06-06 02:34:24 +0300

    upgrade Models User

--- Models/User.php
+++ Models/User.php
@@ -6,9 +6,16 @@ class User
     private function connect()
     {
 
-    require_once('config/dbdata.php');
+    $data = require('config/dbdata.php');
 
-    $db = new PDO("mysql:host={$data['host']};dbname={$data['db']}", $data['user'], $data['password']);
+    try{
+
+        $db = new PDO("mysql:host={$data['host']};dbname={$data['db']}",$data['user'],$data['password']);
+
+    } catch (PDOException $e) {
+
+        die('Подключение не удалось!' . $e->getMessage());
+    }
 
     return $db;
 
@@ -33,17 +40,105 @@ class User
     public function searchUser($login, $password)
     {
 
-	$db = User::connect();
+	    $db = User::connect();
 
-	$sql = "SELECT * FROM users WHERE login = :login AND password = :password";
-	$stmt = $db->prepare($sql);
+	    $sql = "SELECT * FROM users WHERE login = :login AND password = :password";
+	    $stmt = $db->prepare($sql);
 
-	$stmt->execute([
-		'login' => $login,
-		'password' => $password
-	]);
+	    $stmt->execute([
+		    'login' => $login,
+		    'password' => $password
+	    ]);
 
-	return $stmt->fetch(PDO::FETCH_LAZY);
+	    return $stmt->fetch(PDO::FETCH_LAZY);
+
+    }
+
+    public function addCookieUser($type, $userId)
+    {
+
+        $cookie = 'cookie' . $type;
+
+        $db = User::connect();
+
+        $sql = "UPDATE users SET $cookie = $cookie + 1 WHERE id = :id";
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':id', $userId);
+
+        if($stmt->execute())
+        {
+
+            return true;
+
+        }
+        else {
+
+            return false;
+
+        }
+
+    }
+
+    public function getUserId($id)
+    {
+
+        $db = User::connect();
+
+        $sql = "SELECT * FROM users WHERE id = :id";
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':id', $id);
+
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_LAZY);
+
+    }
+
+    public function addCoinsHours($hours, $coinH, $id)
+    {
+
+        $db = User::connect();
+
+        $sql = "UPDATE users SET coins = coins + :hours * :coins WHERE id = :id";
+        $stmt = $db->prepare($sql);
+
+        $stmt->execute([
+            'coins' => $coinH,
+            'hours' => $hours,
+            'id' => $id,
+        ]);
+
+    }
+
+    public function subUserCoins($id, $sub)
+    {
+
+        $db = User::connect();
+
+        $sql = "UPDATE users SET coins = coins - :sub WHERE id = :id";
+        $stmt = $db->prepare($sql);
+
+        $stmt->execute([
+            'id' => $id,
+            'sub' => $sub,
+        ]);
+
+    }
+
+    public function timeNowSet($id, $time)
+    {
+
+        $db = User::connect();
+
+        $sql = "UPDATE users SET date = :time WHERE id = :id";
+        $stmt = $db->prepare($sql);
+
+        $stmt->execute([
+            'time' => $time,
+            'id' => $id
+        ]);
 
     }
 
