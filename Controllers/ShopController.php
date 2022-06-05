538d8a050cb0b6de8c4dda1fae79a0675c0a3bfb
Anton1802 <anton.sup1802@gmail.com>
2022-06-06 02:33:59 +0300

    create ShopController

---
+++ Controllers/ShopController.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once('Models/User.php');
+
+use ErrorMessage;
+use User;
+
+class ShopController
+{
+
+    public function index()
+    {
+
+        if($_SESSION['user'])
+        {
+
+            $userDB = User::getUserId($_SESSION['user']['id']);
+
+            return require_once('layouts/shop.php');
+
+        }
+        else {
+
+            header('Location: /login');
+
+        }
+
+    }
+
+    public function buy($request)
+    {
+
+        $type = trim($request, '/shop/buy/');
+
+        $typePrice = [null, 200, 500, 1000, 2000];
+
+        $userDB = User::getUserId($_SESSION['user']['id']);
+
+        if($userDB->coins >= $typePrice[$type])
+        {
+
+            User::addCookieUser($type, $_SESSION['user']['id']);
+
+            User::subUserCoins($_SESSION['user']['id'], $typePrice[$type]);
+
+            header("Location: /shop");
+
+        }
+        else {
+
+            header("Location: /shop");
+
+        }
+
+    }
+
+}
