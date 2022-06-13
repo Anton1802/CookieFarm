0df02b30a9351892df18a342c0aafa8a0bfbc950
Anton1802 <anton.sup1802@gmail.com>
2022-06-13 04:25:27 +0300

    ajax

--- resource/js/mycookies.js
+++ resource/js/mycookies.js
@@ -1,23 +1,4 @@
 var notyf = new Notyf();
-function noperevod(events) {
-events.preventDefault();
-var href = events.currentTarget['href'];
-$.ajax({
-    url: href,
-    success: function(response){
-        if(response == 1){
-            notyf.success('Success!');
-            setTimeout(function(){
-                document.location.reload(true);
-            }, 2000);
-        }
-        else {
-            notyf.error('Insufficient funds!')
-        }
-    }
-});
-};
-
 jQuery(function (){
 
     $("input").change(function (){
@@ -38,9 +19,6 @@ jQuery(function (){
                 if(response == 1)
                 {
                     notyf.success("Download Success!");
-                    setTimeout(function(){
-                        document.location.reload(true);
-                    }, 2000);
                 }
                 else {
                     notyf.error("Download not Success!");
