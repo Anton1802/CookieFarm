08b3247ebe08399559b6a796376c97c25ed4ee5c
Anton1802 <anton.sup1802@gmail.com>
2022-06-13 04:24:34 +0300

    edit config

--- config/routes.php
+++ config/routes.php
@@ -10,4 +10,5 @@ return [
 '/\/cookies$/' => ['MyController', 'index', []],
 '/\/shop$/' => ['ShopController', 'index', []],
 '/\/shop\/buy\/[1-4]$/' => ['ShopController', 'buy', [$_SERVER['REQUEST_URI']]],
+'/\/avatar$/' => ['MyController', 'downloadAvatar', []],
 ];
